0c64d557a85f44935bd993075304711241481a5a
Victor Grigoryev <o.kak.victor@gmail.com>
2015-04-18 00:29:12 +0300

    Regroup and update #4
    WARNING! DO NOT FORGET TO SET THE FUSES WHILE PROGRAMMING!
    
    -U lfuse:w:0xE1:m -U hfuse:w:0xDD:m -U efuse:w:0xFF:m

---
+++ AVR C source code 2.0/Debug/main.d
@@ -0,0 +1,71 @@
+main.d main.o: .././main.c \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotn85.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotnx5.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\wdt.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\eeprom.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h \
+ ../firmware/usbdrv/usbdrv.h ../firmware/usbdrv/usbconfig.h \
+ ../firmware/usbdrv/usbportability.h ../firmware/usbdrv/oddebug.h
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotn85.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotnx5.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\wdt.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\eeprom.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\soft\atmel\ studio\ 6.2\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h:
+
+../firmware/usbdrv/usbdrv.h:
+
+../firmware/usbdrv/usbconfig.h:
+
+../firmware/usbdrv/usbportability.h:
+
+../firmware/usbdrv/oddebug.h:
